3404cd9ee2c2b246e8ed56a7bfe33cae30929c3f
Babis Karypidis <xabikos@hotmail.com>
2015-05-10 12:12:32 +0300

    Revert "Remove Simple injector nuget packages from Scheduler Server"
    This reverts commit 360d37b4c49fd8c81ac5a9f8234c4a4b7dd057e1.

---
+++ Source/Scheduler.Server/SignalR/SignalRSimpleInjectorDependencyResolver.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.AspNet.SignalR;
+using SimpleInjector;
+
+namespace Scheduler.Server.SignalR
+{
+    /// <summary>
+    /// Custom implementation of <see cref="DefaultDependencyResolver"/> in order to achieve IoC with SignalR and Simple Injector
+    /// </summary>
+    internal class SignalRSimpleInjectorDependencyResolver : DefaultDependencyResolver
+    {
+        private readonly Container _container;
+        public SignalRSimpleInjectorDependencyResolver(Container container)
+        {
+            _container = container;
+        }
+        public override object GetService(Type serviceType)
+        {
+            return ((IServiceProvider)_container).GetService(serviceType) ?? base.GetService(serviceType);
+        }
+
+        public override IEnumerable<object> GetServices(Type serviceType)
+        {
+            return _container.GetAllInstances(serviceType)
+                .Concat(base.GetServices(serviceType));
+        }
+
+    }
+}
--- Source/Scheduler.Server/SignalR/Startup.cs
+++ Source/Scheduler.Server/SignalR/Startup.cs
@@ -5,7 +5,7 @@ using Microsoft.AspNet.SignalR.Infrastructure;
 using Microsoft.Owin.Cors;
 using Owin;
 using Scheduler.Server.Services;
-
+using SimpleInjector;
 
 namespace Scheduler.Server.SignalR
 {
@@ -15,8 +15,31 @@ namespace Scheduler.Server.SignalR
         {
             app.UseCors(CorsOptions.AllowAll);
 
+            var container = new Container();
+
+            container.Register<IConnectedClientsRegistry, ConnectedClientsRegistry>(Lifestyle.Singleton);
+            container.Register<IClientsManager, ClientsManager>(Lifestyle.Transient);
+            container.Register<ClientsHub>(() => new ClientsHub(container.GetInstance<IClientsManager>()));
+            container.Register<IHubContext>(
+                () => container.GetInstance<IConnectionManager>().GetHubContext<ManagementHub>());
+            //container.Register<ManagementHub>(
+            //    () => new ManagementHub(container.GetInstance<IConnectedClientsRegistry>()));
             
-            app.MapSignalR();
+            // This is an extension method from SimpleInjector.Packaging that will scan
+            // all assemblies in the project for IPackage implementations and allow
+            // them to register objects in the container
+            container.RegisterPackages();
+
+            container.Verify();
+
+            var config = new HubConfiguration
+            {
+                Resolver = new SignalRSimpleInjectorDependencyResolver(container)
+            };
+            //var activator = new SimpleInjectorHubActivator(container);
+            //GlobalHost.DependencyResolver.Register(typeof(IHubActivator), () => activator);
+            
+            app.MapSignalR(config);
         }
     }
 }
